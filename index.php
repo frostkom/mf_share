569e0a7cbcfcd28ab0c8a4689482277945256219
frostkom <martin.fors@frostkom.se>
2015-09-15 12:09:58 +0200

    Remove requirement of MF Base

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ Author: Martin Fors
 Author URI: www.frostkom.se
 */
 
-register_activation_hook(__FILE__, 'activate_share');
-
 add_action('init', 'init_share');
 add_action('admin_init', 'settings_share');
 add_action('wp_footer', 'footer_share');
@@ -16,11 +14,6 @@ add_filter('the_content', 'content_share');
 add_shortcode('mf_share', 'shortcode_share');
 
 load_plugin_textdomain('lang_share', false, dirname(plugin_basename(__FILE__)).'/lang/');
-
-function activate_share()
-{
-	require_plugin("mf_base/index.php", "MF Base");
-}
 
 require_once("include/functions.php");
 
