44775798cc860afa75680c35654f78ec910b8748
frostkom <martin.fors@frostkom.se>
2016-04-28 09:54:00 +0200

    Update language file

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Share
 Plugin URI: https://github.com/frostkom/mf_share
 Description: 
-Version: 1.3.6
+Version: 1.3.7
 Author: Martin Fors
 Author URI: http://frostkom.se
 Text Domain: lang_share
