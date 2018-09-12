1f250980efb2613de961234f2ed08a4062e2e929
Martin Fors <martin.fors@frostkom.se>
2018-09-12 15:53:41 +0200

    Update FA icons

--- include/functions.php
+++ include/functions.php
@@ -103,7 +103,7 @@ function setting_share_form_callback()
 
 	$obj_form = new mf_form();
 
-	echo show_select(array('data' => $obj_form->get_for_select(), 'name' => $setting_key, 'value' => $option, 'suffix' => "<a href='".admin_url("admin.php?page=mf_form/create/index.php")."'><i class='fa fa-lg fa-plus'></i></a>"));
+	echo show_select(array('data' => $obj_form->get_for_select(), 'name' => $setting_key, 'value' => $option, 'suffix' => "<a href='".admin_url("admin.php?page=mf_form/create/index.php")."'><i class='fa fa-plus fa-lg'></i></a>"));
 }
 
 function setting_share_options_callback()
@@ -228,29 +228,29 @@ function show_share_services($setting_share_services, $url_to_share)
 
 	if(in_array("facebook", $setting_share_services))
 	{
-		$out .= "<li class='social facebook'><a href='//www.facebook.com/sharer/sharer.php?u=".$url_to_share."' title='".__("Share on", 'lang_share')." Facebook'><i class='fa fa-facebook'></i></a></li>";
+		$out .= "<li class='social facebook'><a href='//www.facebook.com/sharer/sharer.php?u=".$url_to_share."' title='".__("Share on", 'lang_share')." Facebook'><i class='fab fa-facebook-f'></i></a></li>";
 	}
 
 	if(in_array("google-plus", $setting_share_services))
 	{
-		$out .= "<li class='social google-plus'><a href='//plus.google.com/share?url=".$url_to_share."' title='".__("Share on", 'lang_share')." Google+'><i class='fa fa-google-plus'></i></a></li>";
+		$out .= "<li class='social google-plus'><a href='//plus.google.com/share?url=".$url_to_share."' title='".__("Share on", 'lang_share')." Google+'><i class='fab fa-google-plus-g'></i></a></li>";
 	}
 
 	if(in_array("linkedin", $setting_share_services))
 	{
-		$out .= "<li class='social linkedin'><a href='//www.linkedin.com/shareArticle?url=".$url_to_share."&mini=true' title='".__("Share on", 'lang_share')." LinkedIn'><i class='fa fa-linkedin'></i></a></li>";
+		$out .= "<li class='social linkedin'><a href='//www.linkedin.com/shareArticle?url=".$url_to_share."&mini=true' title='".__("Share on", 'lang_share')." LinkedIn'><i class='fab fa-linkedin-in'></i></a></li>";
 		//&source=".$url_to_share."&title=Jonathan%20Suh&summary=Short%20summary
 	}
 
 	if(in_array("pinterest", $setting_share_services))
 	{
-		$out .= "<li class='social pinterest'><a href='//www.pinterest.com/pin/create/button/?url=".$url_to_share."' title='".__("Share on", 'lang_share')." Pinterest'><i class='fa fa-pinterest'></i></a></li>";
+		$out .= "<li class='social pinterest'><a href='//www.pinterest.com/pin/create/button/?url=".$url_to_share."' title='".__("Share on", 'lang_share')." Pinterest'><i class='fab fa-pinterest'></i></a></li>";
 		//&media=https%3A%2F%2Fjonsuh.com%2Ficon.png&description=Short%20description&hashtags=web,development
 	}
 
 	if(in_array("reddit", $setting_share_services))
 	{
-		$out .= "<li class='social reddit'><a href='//www.reddit.com/submit/?url=".$url_to_share."' title='".__("Share on", 'lang_share')." Reddit'><i class='fa fa-reddit'></i></a></li>";
+		$out .= "<li class='social reddit'><a href='//www.reddit.com/submit/?url=".$url_to_share."' title='".__("Share on", 'lang_share')." Reddit'><i class='fab fa-reddit'></i></a></li>";
 	}
 
 	if(in_array("twitter", $setting_share_services))
@@ -259,7 +259,7 @@ function show_share_services($setting_share_services, $url_to_share)
 
 		$out .= "<li class='social twitter'>
 			<a href='//twitter.com/intent/tweet?url=".$url_to_share."' title='".__("Share on", 'lang_share')." Twitter'>
-				<i class='fa fa-twitter'></i>";
+				<i class='fab fa-twitter'></i>";
 
 				if($setting_share_twitter != '')
 				{
@@ -316,7 +316,7 @@ function get_share_content($data = array())
 							$link_extra .= ($link_extra != '' ? "&" : "?")."body=".str_replace("[url]", $url_to_share, $setting_share_email_content);
 						}
 
-						$out .= "<li class='contact email_link'><a href='mailto:".$link_extra."' title='".__("Recommend this page to a friend", 'lang_share')."'>".($setting_share_options_titles == 'yes' ? "<span>".__("Recommend", 'lang_share')."</span>" : "")."<i class='fa fa-envelope'></i></a></li>";
+						$out .= "<li class='contact email_link'><a href='mailto:".$link_extra."' title='".__("Recommend this page to a friend", 'lang_share')."'>".($setting_share_options_titles == 'yes' ? "<span>".__("Recommend", 'lang_share')."</span>" : "")."<i class='far fa-envelope'></i></a></li>";
 					}
 
 					if(in_array("email_form", $setting_share_options))
@@ -325,7 +325,7 @@ function get_share_content($data = array())
 
 						if($form_url != '')
 						{
-							$out .= "<li class='contact email_form'><a href='".$form_url."' title='".__("Recommend this page to a friend", 'lang_share')."'>".($setting_share_options_titles == 'yes' ? "<span>".__("Recommend", 'lang_share')."</span>" : "")."<i class='fa fa-envelope'></i></a></li>";
+							$out .= "<li class='contact email_form'><a href='".$form_url."' title='".__("Recommend this page to a friend", 'lang_share')."'>".($setting_share_options_titles == 'yes' ? "<span>".__("Recommend", 'lang_share')."</span>" : "")."<i class='far fa-envelope'></i></a></li>";
 						}
 					}
 				}
