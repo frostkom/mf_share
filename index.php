b95c70cf7482b7d41a70cce80899b96c95deae80
frostkom <martin.fors@frostkom.se>
2021-03-29 09:26:54 +0200

    Make sure that Base is active

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Share
 Plugin URI: https://github.com/frostkom/mf_share
 Description: 
-Version: 2.5.17
+Version: 2.5.18
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
@@ -14,51 +14,53 @@ Depends: MF Base
 GitHub Plugin URI: frostkom/mf_share
 */
 
-include_once("include/classes.php");
-
-$obj_share = new mf_share();
-
-add_action('cron_base', 'activate_share', mt_rand(1, 10));
-
-if(is_admin())
+if(is_plugin_active("mf_base/index.php"))
 {
-	register_activation_hook(__FILE__, 'activate_share');
-	register_uninstall_hook(__FILE__, 'uninstall_share');
+	include_once("include/classes.php");
 
-	add_action('admin_init', array($obj_share, 'settings_share'));
-	add_action('admin_init', array($obj_share, 'admin_init'), 0);
+	$obj_share = new mf_share();
 
-	add_filter('count_shortcode_button', array($obj_share, 'count_shortcode_button'));
-	add_filter('get_shortcode_output', array($obj_share, 'get_shortcode_output'));
-}
+	add_action('cron_base', 'activate_share', mt_rand(1, 10));
 
-else
-{
-	add_filter('language_attributes', array($obj_share, 'language_attributes'));
+	if(is_admin())
+	{
+		register_activation_hook(__FILE__, 'activate_share');
+		register_uninstall_hook(__FILE__, 'uninstall_share');
 
-	add_action('wp_head', array($obj_share, 'wp_head'), 0);
-	add_action('wp_footer', array($obj_share, 'wp_footer'));
+		add_action('admin_init', array($obj_share, 'settings_share'));
 
-	add_filter('the_content', array($obj_share, 'the_content'));
-	add_filter('the_content_meta', array($obj_share, 'the_content_meta'), 10, 2);
+		add_filter('count_shortcode_button', array($obj_share, 'count_shortcode_button'));
+		add_filter('get_shortcode_output', array($obj_share, 'get_shortcode_output'));
+	}
 
-	add_shortcode('mf_share', array($obj_share, 'shortcode_share'));
-}
+	else
+	{
+		add_filter('language_attributes', array($obj_share, 'language_attributes'));
 
-add_action('widgets_init', array($obj_share, 'widgets_init'));
+		add_action('wp_head', array($obj_share, 'wp_head'), 0);
+		add_action('wp_footer', array($obj_share, 'wp_footer'));
 
-load_plugin_textdomain('lang_share', false, dirname(plugin_basename(__FILE__))."/lang/");
+		add_filter('the_content', array($obj_share, 'the_content'));
+		add_filter('the_content_meta', array($obj_share, 'the_content_meta'), 10, 2);
 
-function activate_share()
-{
-	mf_uninstall_plugin(array(
-		'options' => array('setting_share_form'),
-	));
-}
+		add_shortcode('mf_share', array($obj_share, 'shortcode_share'));
+	}
 
-function uninstall_share()
-{
-	mf_uninstall_plugin(array(
-		'options' => array('setting_share_options', 'setting_share_options_visible', 'setting_share_options_titles', 'setting_share_services', 'setting_share_twitter', 'setting_share_visible', 'setting_share_pages', 'setting_share_form', 'setting_share_email_subject', 'setting_share_email_content'),
-	));
+	add_action('widgets_init', array($obj_share, 'widgets_init'));
+
+	load_plugin_textdomain('lang_share', false, dirname(plugin_basename(__FILE__))."/lang/");
+
+	function activate_share()
+	{
+		mf_uninstall_plugin(array(
+			'options' => array('setting_share_form'),
+		));
+	}
+
+	function uninstall_share()
+	{
+		mf_uninstall_plugin(array(
+			'options' => array('setting_share_options', 'setting_share_options_visible', 'setting_share_options_titles', 'setting_share_services', 'setting_share_twitter', 'setting_share_visible', 'setting_share_pages', 'setting_share_form', 'setting_share_email_subject', 'setting_share_email_content'),
+		));
+	}
 }
