6c2c8d0bf4773bc9d6cd80886e5c6c6f65552cdc
frostkom <martin.fors@frostkom.se>
2025-03-17 12:24:29 +0100

    Make sure that block/widget search result is a number

--- include/classes.php
+++ include/classes.php
@@ -371,7 +371,7 @@ class mf_share
 
 	function wp_head()
 	{
-		if($this->is_correct_page() || apply_filters('get_block_search', 'mf/share') > 0 || !is_plugin_active("mf_widget_logic_select/index.php") || apply_filters('get_widget_search', 'share-widget') > 0)
+		if($this->is_correct_page() || (int)apply_filters('get_block_search', 'mf/share') > 0 || (int)apply_filters('get_widget_search', 'share-widget') > 0)
 		{
 			$plugin_include_url = plugin_dir_url(__FILE__);
 
